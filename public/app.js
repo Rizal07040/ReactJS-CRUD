cb106b68d59b20cd53d18e5899cc8ca2edcc913c
Rizal07040 <adirizal775@gmail.com>
2022-06-08 17:11:52 +0700

    effeck hok,conditional rendering,dan DOM manipulation

--- public/app.js
+++ public/app.js
@@ -7,20 +7,118 @@ const root = document.querySelector('#root'); // // const className = 'box';
 // const element = (
 //     <button onClick={padaSaatAkuDiklik.bind(this, 'Hello, aku diklik!')}>Click Me</button>
 // );
+// function App(){
+//     const [count, setCount] = React.useState(0);
+//     // const count = state[0];
+//     // const updateCount = state[1];
+//     return (
+//         <>
+//         <button 
+//         onClick={function (){
+//             setCount(count - 1);
+//         }}>
+//             -
+//         </button>
+//         <span>{count}</span>
+//         <button 
+//         onClick={function (){
+//             setCount(count + 1);
+//         }}>
+//             +
+//         </button>
+//         </>
+//     )
+// }
+// function App() {
+//     const [diklik, setDiklik] = React.useState(false);
+//     React.useEffect(function (){
+//         console.log(document.getElementById('judul'));
+//     }, );
+//     return (
+//         <>
+//         <h1 id="judul">Hallo ini judul !!</h1>
+//         <button
+//         onClick={
+//             function(){
+//                 setDiklik(true);
+//             }
+//         }>
+//             Klik aku dong
+//         </button>
+//         </>
+//         );
+// }
+// setTimeout(function(){
+//     ReactDOM.render(<App />, root);
+// },1000);
+//  useEffec
+// function App() {
+//     const [login, setLogin] = React.useState(false);
+//     if (login) {
+//         return (
+//             <>
+//             <h1>Udah Login, Bro!</h1>
+//             <button onClick={function(){
+//                 setLogin(false);
+//             }}>LogOut</button>
+//             </>
+//         );
+//     }
+//     return (
+//         <>
+//         <h1>Login dulu, Bang!</h1>
+//         <button
+//         onClick={function (){
+//             setLogin(true);
+//         }}>Login</button>
+//         </>
+//     )
+// }
+// Conditional Rendering
+// function App() {
+//     const [login, setLogin] = React.useState(false);
+//     return (
+//         <>
+//         <h1>Application</h1>
+//         <p>{login ? <b>Kamu sudah login</b> : <i>Kamu belum login!!!</i>}</p>
+//         <button
+//             onClick={function (){
+//                 setLogin(true);
+//             }}>
+//                 Login
+//         </button>
+//         </>
+//     );
+// }
+// function App() {
+//     const [login, setLogin] = React.useState(false);
+//     return (
+//         <>
+//         <h1>Application</h1>
+//         {/* <p>{login && <b>Kamu sudah login !!</b> }</p> */}
+//         <p>{login ==true && <b>Kamu sudah login !!</b> }</p>
+//         <button
+//             onClick={function (){
+//                 setLogin(true);
+//             }}>
+//                 Login
+//         </button>
+//         </>
+//     );
+// }
+// DOM Manipulation
 
 function App() {
-  const [count, setCount] = React.useState(0); // const count = state[0];
-  // const updateCount = state[1];
-
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setCount(count - 1);
-    }
-  }, "-"), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
-    onClick: function () {
-      setCount(count + 1);
-    }
-  }, "+"));
+  const [login, setLogin] = React.useState(false);
+  const judulRef = React.useRef(null);
+  React.useEffect(function () {
+    setTimeout(function () {
+      judulRef.current.textContent = 'Aplikasi';
+    }, 1000);
+  }, []);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    ref: judulRef
+  }, "Application"));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -12,33 +12,134 @@ const root = document.querySelector('#root');
 //     <button onClick={padaSaatAkuDiklik.bind(this, 'Hello, aku diklik!')}>Click Me</button>
 // );
 
-//function App(){
-    // const [count, setCount] = React.useState(0);
+// function App(){
+//     const [count, setCount] = React.useState(0);
     
-    // // const count = state[0];
-    // // const updateCount = state[1];
+//     // const count = state[0];
+//     // const updateCount = state[1];
     
-    // return (
-    //     <>
-    //     <button 
-    //     onClick={function (){
-    //         setCount(count - 1);
-    //     }}>
-    //         -
-    //     </button>
-    //     <span>{count}</span>
-    //     <button 
-    //     onClick={function (){
-    //         setCount(count + 1);
-    //     }}>
-    //         +
-    //     </button>
-    //     </>
-    // )
-//}
+//     return (
+//         <>
+//         <button 
+//         onClick={function (){
+//             setCount(count - 1);
+//         }}>
+//             -
+//         </button>
+//         <span>{count}</span>
+//         <button 
+//         onClick={function (){
+//             setCount(count + 1);
+//         }}>
+//             +
+//         </button>
+//         </>
+//     )
+// }
 
+// function App() {
+//     const [diklik, setDiklik] = React.useState(false);
+
+//     React.useEffect(function (){
+//         console.log(document.getElementById('judul'));
+//     }, );
+//     return (
+//         <>
+//         <h1 id="judul">Hallo ini judul !!</h1>
+//         <button
+//         onClick={
+//             function(){
+//                 setDiklik(true);
+//             }
+//         }>
+//             Klik aku dong
+//         </button>
+//         </>
+//         );
+// }
+
+// setTimeout(function(){
+//     ReactDOM.render(<App />, root);
+
+// },1000);
+
+//  useEffec
+// function App() {
+//     const [login, setLogin] = React.useState(false);
+
+//     if (login) {
+//         return (
+//             <>
+//             <h1>Udah Login, Bro!</h1>
+//             <button onClick={function(){
+//                 setLogin(false);
+//             }}>LogOut</button>
+//             </>
+//         );
+//     }
+
+//     return (
+//         <>
+//         <h1>Login dulu, Bang!</h1>
+//         <button
+//         onClick={function (){
+//             setLogin(true);
+//         }}>Login</button>
+//         </>
+//     )
+// }
+// Conditional Rendering
+// function App() {
+//     const [login, setLogin] = React.useState(false);
+
+//     return (
+//         <>
+//         <h1>Application</h1>
+//         <p>{login ? <b>Kamu sudah login</b> : <i>Kamu belum login!!!</i>}</p>
+//         <button
+//             onClick={function (){
+//                 setLogin(true);
+//             }}>
+//                 Login
+//         </button>
+//         </>
+//     );
+// }
+// function App() {
+//     const [login, setLogin] = React.useState(false);
+
+//     return (
+//         <>
+//         <h1>Application</h1>
+//         {/* <p>{login && <b>Kamu sudah login !!</b> }</p> */}
+//         <p>{login ==true && <b>Kamu sudah login !!</b> }</p>
+//         <button
+//             onClick={function (){
+//                 setLogin(true);
+//             }}>
+//                 Login
+//         </button>
+//         </>
+//     );
+// }
+
+// DOM Manipulation
 function App() {
-    
+    const [login, setLogin] = React.useState(false);
+    const judulRef = React.useRef(null);
+
+    React.useEffect(function () {
+        setTimeout(function () {
+            judulRef.current.textContent = 'Aplikasi';
+
+        },1000);
+    },[]);
+
+    return (
+        <>
+        <h1 ref = {judulRef}>Application</h1>
+                </>
+    );
 }
 
 ReactDOM.render(<App />, root);
